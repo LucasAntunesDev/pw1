18c8cd5831ae4bdb9ab790b93ddaf12dd13193c7
Lucas Antunes de Lara <lucasantuneslara06@gmail.com>
2022-07-22 19:18:29 -0300

    Aula 22/07/22

---
+++ Javascript/Exercícios/ex5.js
@@ -0,0 +1,23 @@
+/*
+Faça um script que receba uma nota de cada trimestre. 
+Calcule a média e escreva:
+Aprovado - se a média for igual ou maior que 7
+Exame - se a média for igual ou maior que 5
+Reprovado, caso contário
+*/ 
+
+var nota1 = parseFloat(prompt("Digite a nota do 1º trimestre: "))
+var nota2 = parseFloat(prompt("Digite a nota do 2º trimestre: "))
+var nota3 = parseFloat(prompt("Digite a nota do 3º trimestre: "))
+var media = (nota1 + nota2 + nota3) / 3
+
+console.log(media)
+
+if(media >= 7){
+    alert("Aprovado")
+}
+else if(media >= 5){
+    alert("Exame")
+}else{
+    alert("Reprovado")
+}
